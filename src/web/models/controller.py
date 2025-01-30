05de2c6ef174c72b297a8f681f405534bac61ba0
Beresnev Aleksei <meikuam@gmail.com>
2025-01-30 23:28:06 +0700

    remove deprecated code

--- src/web/models/controller.py
+++
@@ -1,6 +0,0 @@
-from typing import List
-from pydantic import BaseModel
-
-
-class ControllerIds(BaseModel):
-    controller_ids: List[int]
--- src/web/models/leds.py
+++
@@ -1,7 +0,0 @@
-from typing import Optional
-from pydantic import BaseModel
-
-
-class Leds(BaseModel):
-    state: Optional[bool]
-    value: Optional[int]
